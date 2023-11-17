a6daa95d2a17edf3cc6cfea4cba5735ba09448b6
Deceptrax123 <srinitishsri@gmail.com>
2023-11-17 17:21:30 +0530

    first commit

---
+++ boxes.py
@@ -0,0 +1,8 @@
+import torch 
+import pandas as pd 
+import numpy as np 
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import os 
+from dotenv import load_dotenv
+import cv2
